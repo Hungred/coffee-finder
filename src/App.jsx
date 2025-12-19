508c5029c8cf7633c56e5934333a7c348995271c
Anita <anita@AnitadeMacBook-Pro.local>
2025-12-19 16:37:53 +0800

    feat: 安裝Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='min-h-screen bg-coffee-yellow p-6'>
+        <div className='bg-white rounded-card shadow-soft p-4'>
+          <h1 className='text-dark text-xl font-bold'>
+            Coffee Finder 測試成功！
+          </h1>
+          <p className='text-coffee-green-light'>
+            這是一個使用了 Tailwind v4 的測試卡片。
+          </p>
+          <button className='mt-4 bg-coffee-green px-4 py-2 rounded-full'>
+            探索咖啡廳
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
