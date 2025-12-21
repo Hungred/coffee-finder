99c1d6ce0458d864b26f4beb2a24aa0c98bd1327
Anita <anita397892@gamil.com>
2025-12-21 15:17:42 +0800

    feat: 新增城市假資料

---
+++ src/data/cities.ts
@@ -0,0 +1,58 @@
+import type { City } from '../types/city.js';
+
+export const CITIES: City[] = [
+  {
+    id: 'taipei',
+    name: '台北市',
+    engName: 'Taipei',
+    center: [25.033, 121.5654],
+    count: 3,
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuCoRW7NIo9jLZfU-qeC_UpAgJTZReBJre0CDw6aHmv7cZXkE3uJjKMz_S2xQgm5EEKFagkHK5BqTaGjfr2zMewMh1xOsamMp2gGnH9Tnd2IU6p2GlyknQab3jtwZMSQFufwyhfMaN8D2VdJO4Ls9MaGM_aYe1qSsRfW5tTGMKRAg6zDNpZx_I5w7Y5xhATFpKqozwNn1Ch48cEzEA7T1aj3ciL7l7fHWiDO-NE_0FKfyVKuAw7XEq2tHIM__r2mpP6UiuXar7_sRw',
+  },
+  {
+    id: 'taichung',
+    name: '台中市',
+    engName: 'Taichung',
+    center: [24.1477, 120.6736],
+    count: 33,
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuAEasGpWOA3NU0LF0WnijDBInc5YDI_RSWOLTp_cgohbXl-dtU4SHYfrQZXiO6Bo93XTgoHvnmxaeJ6eud-prFQGmYUR6wbGgwT-3Hp018cXpBgoazJQpNdxiSw8MSj_p42OvQdPkfDBqMV8w2xGydu5EcdPX1HKuhyxXYoIIduWX6ajNz_R_8aQkci2hK4RSahfA0Wld2YW0iwVjQRcuKg2pB0eaPlxI3FYa1cGkppN4pVnrRde1b0HXWsKp5p5oq3s_tJJ5WF_A',
+  },
+  {
+    id: 'kaohsiung',
+    name: '高雄市',
+    engName: 'Kaohsiung',
+    center: [22.6273, 120.3014],
+    count: 10,
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuAHA6bVPeNA93xScyqW4XG97lmNnYW59W4y4Z3hMTnnQNHgBpj9_946RgdGWA65d0_HCV-34nLUAB5oFyjffrh7yvvDmt2Yu72QlQlawgRrjuvcpbIsOFB7TuuD1poow0v9MqFgk_sVsZ3rePX34z0Ne7d8xfU2CdHTX1-6-ce-pNeFUXoI3ho2N9c2oH-NRzL38PFOtgP_XgQ5wqVHVFGRfoUJPYIDZPzX-K16f2ARiNUlOsJgRp5504cjoz49A-A4vHvfqSkElg',
+  },
+  {
+    id: 'kaohsiung2',
+    name: '高雄市2',
+    engName: 'Kaohsiung2',
+    center: [22.6273, 120.3014],
+    count: 10,
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuAHA6bVPeNA93xScyqW4XG97lmNnYW59W4y4Z3hMTnnQNHgBpj9_946RgdGWA65d0_HCV-34nLUAB5oFyjffrh7yvvDmt2Yu72QlQlawgRrjuvcpbIsOFB7TuuD1poow0v9MqFgk_sVsZ3rePX34z0Ne7d8xfU2CdHTX1-6-ce-pNeFUXoI3ho2N9c2oH-NRzL38PFOtgP_XgQ5wqVHVFGRfoUJPYIDZPzX-K16f2ARiNUlOsJgRp5504cjoz49A-A4vHvfqSkElg',
+  },
+  {
+    id: 'kaohsiung3',
+    name: '高雄市3',
+    engName: 'Kaohsiung3',
+    center: [22.6273, 120.3014],
+    count: 10,
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuAHA6bVPeNA93xScyqW4XG97lmNnYW59W4y4Z3hMTnnQNHgBpj9_946RgdGWA65d0_HCV-34nLUAB5oFyjffrh7yvvDmt2Yu72QlQlawgRrjuvcpbIsOFB7TuuD1poow0v9MqFgk_sVsZ3rePX34z0Ne7d8xfU2CdHTX1-6-ce-pNeFUXoI3ho2N9c2oH-NRzL38PFOtgP_XgQ5wqVHVFGRfoUJPYIDZPzX-K16f2ARiNUlOsJgRp5504cjoz49A-A4vHvfqSkElg',
+  },
+  {
+    id: 'kaohsiung4',
+    name: '高雄市4',
+    engName: 'Kaohsiung4',
+    center: [22.6273, 120.3014],
+    count: 10,
+    image:
+      'https://lh3.googleusercontent.com/aida-public/AB6AXuAHA6bVPeNA93xScyqW4XG97lmNnYW59W4y4Z3hMTnnQNHgBpj9_946RgdGWA65d0_HCV-34nLUAB5oFyjffrh7yvvDmt2Yu72QlQlawgRrjuvcpbIsOFB7TuuD1poow0v9MqFgk_sVsZ3rePX34z0Ne7d8xfU2CdHTX1-6-ce-pNeFUXoI3ho2N9c2oH-NRzL38PFOtgP_XgQ5wqVHVFGRfoUJPYIDZPzX-K16f2ARiNUlOsJgRp5504cjoz49A-A4vHvfqSkElg',
+  },
+];
