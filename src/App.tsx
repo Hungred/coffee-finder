9b41471f691766cc88ffdc8d0f95d9636513a8c5
Hungred <anita397892@gmail.com>
2025-12-21 20:57:53 +0800

    feat: 部署到github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.js';
 import MapView from './pages/MapView.js';
 import City from './pages/City.js';
