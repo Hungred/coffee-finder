16f158769167680ce4598bc345b9fd5cf32a8f9a
Hungred <anita397892@gmail.com>
2025-12-22 18:08:01 +0800

    feat: 新增收藏頁，加上context 儲存所有收藏的咖啡廳

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,32 @@ import Home from './pages/Home.js';
 import MapView from './pages/MapView.js';
 import City from './pages/City.js';
 import CityList from './pages/CityList.js';
-// import Favorites from './pages/Favorites.js';
+import Favorites from './pages/Favorites.js';
 import BottomNav from './components/BottomNav.js';
+import { FavoriteProvider } from './context/FavoriteContext.js';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className='max-w-md mx-auto h-[100dvh] flex flex-col bg-coffee-bg-light shadow-2xl relative overflow-hidden'>
-        <main className='flex-1 relative min-h-0'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/map' element={<MapView />} />
-            <Route path='/city' element={<City />} />
-            <Route path='/city/:cityId' element={<CityList />} />
-            {/* <Route path='/favorites' element={<Favorites />} /> */}
-          </Routes>
-        </main>
+    <FavoriteProvider>
+      <Router>
+        <div className='max-w-md mx-auto h-[100dvh] flex flex-col bg-coffee-bg-light shadow-2xl relative overflow-hidden'>
+          <main className='flex-1 relative min-h-0'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/map' element={<MapView />} />
+              <Route path='/city' element={<City />} />
+              <Route path='/city/:cityId' element={<CityList />} />
+              <Route path='/favorites' element={<Favorites />} />
+            </Routes>
+          </main>
 
-        {/* 底部導覽列：固定 */}
-        <footer className='flex-none border-t border-gray-100 bg-white'>
-          <BottomNav />
-        </footer>
-      </div>
-    </Router>
+          {/* 底部導覽列：固定 */}
+          <footer className='flex-none border-t border-gray-100 bg-white'>
+            <BottomNav />
+          </footer>
+        </div>
+      </Router>
+    </FavoriteProvider>
   );
 };
 
