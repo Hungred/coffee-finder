114f649882f06c7644aeed03245ef85a6f1ff557
Hungred <anita397892@gmail.com>
2025-12-25 15:34:31 +0800

    modify: 修改api 寫法

---
+++ src/services/api.ts
@@ -0,0 +1,43 @@
+import type { Cafe } from '../types/cafe.js';
+import http from './http.js';
+const transformCafe = (item: any): Cafe => ({
+  ...item,
+  latitude: parseFloat(item.latitude),
+  longitude: parseFloat(item.longitude),
+
+  position: [parseFloat(item.latitude), parseFloat(item.longitude)],
+  tags: [
+    item.wifi >= 4 ? 'WiFi穩' : '',
+    item.quiet >= 4 ? '安靜' : '',
+    item.seat >= 4 ? '位子多' : '',
+    item.limited_time === 'no' ? '不限時' : '',
+  ].filter(Boolean),
+  isFavorite: false,
+  image: `https://images.unsplash.com/photo-1509042239860-f550ce710b93?w=500&sig=${item.id}`,
+  score: (item.wifi + item.seat + item.quiet + item.tasty) / 4,
+});
+
+export const fetchCafes = async (payload?: {
+  searchQuery?: string;
+  tags?: string[];
+}): Promise<Cafe[]> => {
+  try {
+    const rawData = await http.get<Cafe[]>('', payload);
+
+    return rawData.map(transformCafe);
+  } catch (error) {
+    console.error('獲取咖啡廳資料失敗', error);
+    return [];
+  }
+};
+
+export const fetchCafesById = async (id: string): Promise<Cafe | null> => {
+  try {
+    const rawData = await http.get(`/${id}`);
+
+    return transformCafe(rawData);
+  } catch (error) {
+    console.error('獲取咖啡廳資料失敗', error);
+    return null;
+  }
+};
