81b8b7bc16e10210795d25b97772dd4a83cbd913
Hungred <anita397892@gmail.com>
2025-12-30 13:02:16 +0800

    feat: 新增城市圖片

---
+++ src/tools/const.ts
@@ -0,0 +1,13 @@
+import changhua from '../assets/changhua.jpg';
+import chiayi from '../assets/chiayi.jpg';
+import hsinchu from '../assets/hsinchu.jpg';
+import hualien from '../assets/hualien.jpg';
+import taipei from '../assets/taipei.jpg';
+
+export const CITY_IMAGE_MAP: Record<string, string> = {
+  changhua: changhua,
+  chiayi: chiayi,
+  hsinchu: hsinchu,
+  hualien: hualien,
+  taipei: taipei,
+};
